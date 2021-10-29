36c9ec452000affa9870cf37dd74c540f227c35a
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-29 16:23:15 +0200

    [add] afterinit handle disconnect

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -18,4 +18,25 @@ export class AppGateway implements OnGatewayInit, OnGatewayDisconnect, OnGateway
   get wss(): Server {
     return this._wss;
   }
+
+
+  afterInit(server: Server) {
+    this.logger.log("Init!");
+    this.wss.emit("msgToClient", "Init!");
+    this.FIELD_SIZE = 400;
+  }
+
+  async handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Connected! ${client.id}`);
+  }
+
+
+  /**
+   * logs disconnect
+   * @param client
+   */
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Disconnected! ${client.id}`);
+  }
+
 }
