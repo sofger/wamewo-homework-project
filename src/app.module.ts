74459ce2f29c652bbd34cfdb23d862dd313e0531
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-29 16:44:20 +0200

    [update] app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AppGateway } from "./app.gateway";
+import { WolfModule } from "./colony/wolf/wolf.module";
+import { SheepModule } from "./colony/sheep/sheep.module";
 
 @Module({
-  imports: [],
+  imports: [SheepModule, WolfModule],
   controllers: [AppController],
   providers: [AppService, AppGateway]
 })
