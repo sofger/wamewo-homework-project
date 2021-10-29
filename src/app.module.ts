04fe5517a1ccf40fce7f130c256c3050bb0bb805
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-29 16:25:30 +0200

    [refactor] linter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 import { AppGateway } from "./app.gateway";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService,AppGateway],
+  providers: [AppService, AppGateway]
 })
-export class AppModule {}
+export class AppModule {
+}
