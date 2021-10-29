1562f835baa0f12feec32f3882bbad38c726151c
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-29 16:32:38 +0200

    [add] animal base model

---
+++ src/colony/Animal.abstract.ts
@@ -0,0 +1,76 @@
+import { Coordinates } from "../coordinates/coordinates.model";
+import { Direction } from "../coordinates/direction.enum";
+
+export abstract class Animal {
+  protected _position: Coordinates;
+
+  protected constructor(id: number, position: Coordinates) {
+    this._id = id;
+    this._position = position;
+  }
+
+  protected _id: number;
+
+  get id(): number {
+    return this._id;
+  }
+
+  get getPosition(): Coordinates {
+    return this._position;
+  }
+
+  set setPosition(value: Coordinates) {
+    this._position = value;
+  }
+
+  move(speed, FIELD_WIDTH: number, direction: Direction): void {
+    switch (direction) {
+      case Direction.UP_RIGHT:
+        if (this.getPosition.y > 1 || this.getPosition.x < 1) {
+          this.getPosition.y -= speed;
+          this.getPosition.x += speed;
+        }
+        break;
+      case Direction.UP_LEFT:
+        if (this.getPosition.y > 1 || this.getPosition.x < 1) {
+          this.getPosition.y -= speed;
+          this.getPosition.x -= speed;
+        }
+        break;
+
+      case Direction.DOWN_LEFT:
+        if (this.getPosition.y < FIELD_WIDTH || this.getPosition.x < FIELD_WIDTH) {
+          this.getPosition.y += speed;
+          this.getPosition.x -= speed;
+        }
+        break;
+      case Direction.DOWN_RIGHT:
+        if (this.getPosition.y < FIELD_WIDTH || this.getPosition.x < FIELD_WIDTH) {
+          this.getPosition.y += speed;
+          this.getPosition.x += speed;
+        }
+        break;
+      case Direction.LEFT:
+        if (this.getPosition.x > 1) {
+          this.getPosition.x -= speed;
+        }
+        break;
+      case Direction.RIGHT:
+        if (this.getPosition.x < FIELD_WIDTH) {
+          this.getPosition.x += speed;
+        }
+        break;
+      case Direction.UP:
+        if (this.getPosition.y > 1) {
+          this.getPosition.y -= speed;
+        }
+        break;
+      case Direction.DOWN:
+        if (this.getPosition.y < FIELD_WIDTH) {
+          this.getPosition.y += speed;
+        }
+        break;
+    }
+  }
+
+}
