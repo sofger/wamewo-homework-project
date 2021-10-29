319511d76aa31fff76e2b3f39a280fdd72f60125
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-29 16:31:41 +0200

    [add] sheep model

---
+++ src/colony/sheep/sheep.model.ts
@@ -0,0 +1,21 @@
+import { Coordinates } from "../../coordinates/coordinates.model";
+import { Animal } from "../Animal.super";
+
+
+export class SheepModel extends Animal {
+  private _isDead: boolean;
+
+  constructor(sheepId: number, coordinate: Coordinates) {
+    super(sheepId, coordinate);
+    this._isDead = false;
+  }
+
+  get dead(): boolean {
+    return this._isDead;
+  }
+
+  set dead(value: boolean) {
+    this._isDead = value;
+  }
+
+}
